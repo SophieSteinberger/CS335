ee80262fa47345926fd29e5cea9d4bd36b937a37
SophieSteinberger <156860341+SophieSteinberger@users.noreply.github.com>
2024-05-14 11:06:44 -0500

    tying to commit to the new branch

--- testProject/src/testProject/Tester.java
+++ testProject/src/testProject/Tester.java
@@ -3,7 +3,8 @@ package testProject;
 public class Tester {
 	public static void main(String[] args) {
 		System.out.println("Testing!");
-		System.out.println("It worked--let's do it again!");
+		System.out.println("It worked!");
+		System.out.println("Is this branch working?");
 	}
 
 }
